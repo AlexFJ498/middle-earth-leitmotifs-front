851c8783f53987576189b553a703fec02b12cdac
AlexFJ498 <alejfuejur@gmail.com>
2026-01-25 21:05:50 +0100

    feat: add handling for Sauron and The Evil of the Ring theme ID to show correct tracks

--- src/infrastructure/ApiTrackThemeRepository.ts
+++ src/infrastructure/ApiTrackThemeRepository.ts
@@ -30,6 +30,13 @@ export class ApiTrackThemeRepository {
 	}
 
 	async findByTheme(themeId: string) {
+		// Exceptional case: Sauron and The Evil of the Ring are essentially the same theme, but they need
+		// to be treated differently in the Themes section. When fetching tracks for Sauron, we need to use
+		// The Evil of the Ring's ID to actually get the tracks, so they don't get duplicated in the Tracks list.
+		if (themeId === import.meta.env.THE_EVIL_OF_THE_RING_UUID) {
+			themeId = import.meta.env.MORDOR_SAURON_UUID;
+		}
+
 		const response = await fetch(`${this.baseUrl}/themes/${themeId}/tracks`, {
 			headers: { "Content-Type": "application/json" },
 		});
